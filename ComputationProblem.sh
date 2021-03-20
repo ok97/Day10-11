a3f8773c701d854ee10a9b9b669f1e60ce335319
ok97 <omkhawshi0@gmail.com>
2021-03-20 23:13:35 +0530

    Refactored : Compute a%b+c programs

--- ComputationProblem.sh
+++ ComputationProblem.sh
@@ -5,6 +5,8 @@ echo "You Enter Inputs Is :- " $a, $b, $c
 Compute_2=$((a + b * c))
 Compute_3=$((a * b + c))
 Compute_4=$((c + a / b))
+Compute_5=$((a % b + c))
 echo "Compute (a+b*c):-"$Compute_2
 echo "Compute (a*b+c):-"$Compute_3
 echo "Compute (c+a/b):-"$Compute_4
+echo "Compute (a%b+c):-"$Compute_5
