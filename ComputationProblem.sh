be6abd505b65e90799fe8b52016cf6783736b5fb
ok97 <omkhawshi0@gmail.com>
2021-03-20 23:03:31 +0530

    Refactored : Compute a*b+c programs

--- ComputationProblem.sh
+++ ComputationProblem.sh
@@ -2,6 +2,8 @@ read -p "Enter First Input :- " a
 read -p "Enter Second Input :- " b
 read -p "Enter Third Input :- " c
 echo "You Enter Inputs Is :- " $a, $b, $c
-Compute=$((a + b * c))
-echo "Compute (a+b*c):-"$Compute
+Compute_1=$((a + b * c))
+Compute_2=$((a * b + c))
+echo "Compute (a+b*c):-"$Compute_1
+echo "Compute (a*b+c):-"$Compute_1
 
