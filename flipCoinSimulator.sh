620ff142510e2d8d64674229dc89f272cfe1fc56
ok97 <omkhawshi0@gmail.com>
2021-03-21 23:19:13 +0530

    Refactored : Won 21 Times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,10 +2,10 @@ RANDOM=$$
 Hcount=0
 Tcount=0
 i=1
-b=6
-while [ $Hcount -le 6 ] | [ $Tcount -le 6 ]
+b=21
+while [ $Hcount -le 21 ] | [ $Tcount -le 21 ]
 do
-r1=$(expr $RANDOM % 5)
+r1=$(expr $RANDOM % 2)
 r2=$(expr $r1 % 2)
 if [ $r2 -eq 0 ]
 then
@@ -16,13 +16,13 @@ then
 echo -ne "Tail \n"
 Tcount=$(expr $Tcount + 1)
 fi
-if [ $Hcount -eq 6 ]
+if [ $Hcount -eq 21 ]
 then
-echo "☺☺☺☺☺☺☺☺ Head Wins 6 Times ☺☺☺☺☺☺☺☺"
+echo "☺☺☺☺☺☺☺☺ Head Wins 21 Times ☺☺☺☺☺☺☺☺"
 break
-elif [ $Tcount -eq 6 ]
+elif [ $Tcount -eq 21 ]
 then
-echo "☺☺☺☺☺☺☺☺ Tails wins 6 Times ☺☺☺☺☺☺☺☺"
+echo "☺☺☺☺☺☺☺☺ Tails wins 21 Times ☺☺☺☺☺☺☺☺"
 break
 fi
 i=$(expr $i + 1 )
